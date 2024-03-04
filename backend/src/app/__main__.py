ce10f2baa93eaf8143d3ac299a3fe56e38f6680b
tibov <tibo.verreycken@telenet.be>
2024-03-04 22:14:58 +0100

    force push to save production, hopefully

--- backend/src/app/__main__.py
+++ backend/src/app/__main__.py
@@ -3,8 +3,8 @@ from confz import FileSource
 
 from .app import init_app, APIConfig
 
+config = APIConfig(config_sources=FileSource(file='config.yml'))
+app = init_app(config)
 
 if __name__ == "__main__":
-    config = APIConfig(config_sources=FileSource(file='config.yml'))
     uvicorn.run(app=init_app(config), host="0.0.0.0", port=8000)
-

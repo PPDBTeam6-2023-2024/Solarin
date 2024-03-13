ecb810ec0b5cd61569f249a4eb8824dbe77dea49
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2024-03-13 11:20:00 +0100

    FEAT: region generation by cells

--- backend/src/app/logic/planet_generation/planet_generation.py
+++ backend/src/app/logic/planet_generation/planet_generation.py
@@ -13,7 +13,21 @@ def generate_regions(
         region_types: list[enums.RegionType],
         region_scale: float
 ) -> list[datatypes.Region]:
-    return []
+    regions = []
+
+    cell_width = int(width * region_scale)
+    cell_height = int(height * region_scale)
+
+    for j in range(0, height, cell_height):
+        for i in range(0, width, cell_width):
+            x = random.randint(i, i+cell_width)
+            y = random.randint(j, j+cell_height)
+            region = datatypes.Region(
+                type=random.choice(region_types),
+                coordinate=(x, y)
+            )
+            regions.append(region)
+    return regions
 
 
 def get_distance_formula(

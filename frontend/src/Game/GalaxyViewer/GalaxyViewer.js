91d8c8c40600b102bf97afd8b985138994f26ebc
magnustymoteus <patryk.pilichowski@student.uantwerpen.be>
2024-03-16 17:04:48 +0100

    sidemenu with settings window and sign out option

--- frontend/src/Game/GalaxyViewer/GalaxyViewer.js
+++ frontend/src/Game/GalaxyViewer/GalaxyViewer.js
@@ -1,13 +1,11 @@
 import { useLoader, Canvas, } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { OrbitControls } from '@react-three/drei'
-import { WebGL1Renderer } from 'three'
 
 
 function GalaxyViewer(props) {
     const planetMap = useLoader(TextureLoader, props.mapImage)
     return (
-        <WebGL1Renderer>
         <Canvas style={{position: "fixed"}}>
         <ambientLight intensity={1} />
         <OrbitControls autoRotate autoRotateSpeed={0.1} enableZoom={false} enablePan={false} enableRotate/>
@@ -16,7 +14,6 @@ function GalaxyViewer(props) {
       <meshStandardMaterial map={planetMap}/>
         </mesh>
       </Canvas>
-      </WebGL1Renderer>
     )
 
 }

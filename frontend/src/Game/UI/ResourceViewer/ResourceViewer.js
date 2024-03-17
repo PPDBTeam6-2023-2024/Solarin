1e0193e3b73076ff2c0750855b42838f632f2d38
magnustymoteus <patryk.pilichowski@student.uantwerpen.be>
2024-03-17 12:14:44 +0100

    resource collection rate visualization

--- frontend/src/Game/UI/ResourceViewer/ResourceViewer.js
+++ frontend/src/Game/UI/ResourceViewer/ResourceViewer.js
@@ -8,10 +8,11 @@ const getResourceStats = (resources, resourcesInfo) => {
     for (let resource in resources){
         elements.push(
             <Tooltip key={resource} title={resourcesInfo[resource]["description"]}>
-            <div className="flex items-center justify-center mr-3 bg-gradient-to-r from-gray-600 to-gray-700 p-1">
-            <p className="whitespace-pre-wrap">{resources[resource]}
+            <div className="mr-3 bg-gradient-to-r from-gray-600 to-gray-700 p-1">
+            <p>{resources[resource]["collected"]}
             <img className="inline ml-2 h-6 w-6"src={(`/images/resources/${resourcesInfo[resource]["icon"]}`)} alt={resource} draggable={false}/>
             </p>
+            {resources[resource]["producing"] && <p className="text-xs">{resources[resource]["producing"]}/hr</p>}
             </div>
             </Tooltip>
         )

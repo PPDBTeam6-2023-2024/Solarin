66ff1a266e2042fb66883f6b0c8b11c91266cb41
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2024-03-19 16:24:09 +0100

    planet view begin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Login from './Login/Login'
-import Game from './Game/Game'
+import PlanetViewer from './Game/PlanetViewer/PlanetViewer';
+
+function generateData(width, height) {
+  const data = [];
+  const cellWidth = 1 / width;
+  const cellHeight = 1 / height;
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      const x = Math.random() * cellWidth + j * cellWidth;
+      const y = Math.random() * cellHeight + i * cellHeight;
+      const types = ['type1', 'type2', 'type3']
+      const regionType = types[Math.floor(Math.random()*types.length)];
+      data.push({ x, y, regionType });
+    }
+  }
+
+  return data;
+}
+
 function App() {  
+  const data = generateData(5, 5);
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/game" element={<Game />}/>
-        <Route path="*" element={<Navigate to="/"/>}/>
-      </Routes>
-    </BrowserRouter>
+      <PlanetViewer data={data} width={400} height={400} mapImage='./Game/Images/Planets/example.png' planetName="test"/>
     </div>
   );
 }
--- frontend/src/Game/PlanetViewer/PlanetViewer.js
+++ frontend/src/Game/PlanetViewer/PlanetViewer.js
@@ -1,179 +1,58 @@
-import { MapInteractionCSS } from 'react-map-interaction';
-import {useState, useEffect, useContext} from 'react';
-import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
-import getCities from './CityViewer/getCities';
-import CityManager from "./CityViewer/cityManager";
-import {PlanetListContext} from "./../Context/PlanetListContext"
-import ArmyViewer from '../UI/armyViewer/armyViewer'
-import getArmies from "./getArmies";
+import { useMemo } from 'react';
+import { Delaunay } from 'd3'; 
+import * as d3 from "d3";
 
-const loadImage = async (imgPath, stateSetter) => {
-    let img = new Image()
-    img.src = imgPath
-    img.onload = () => {
-        stateSetter(img)
-    }
+function getImagePath(regionType) {
+    const imagePaths = {
+        type1: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+        type2: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
+        type3: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+    };
+
+    return imagePaths[regionType] || '.Game/Images/Planets/example.png';
 }
 
 function PlanetViewer(props) {
+    const xScale = d3.scaleLinear().domain([0, 1]).range([0, props.width]);
+    const yScale = d3.scaleLinear().domain([0, 1]).range([0, props.height]);
 
-    const [mapState, setMapState] = useState({
-        scale: 1,
-        translation: {x: 0, y: 0},
-    });
-    const [image, setImage] = useState();
-    const [armyImages, setArmyImages] = useState([]);
-    const [activeArmyViewers, setActiveArmyViewers] = useState([]);
-    const [updateTrigger, setUpdateTrigger] = useState(false);
+    const delaunay = useMemo(() => {
+        const formattedData = props.data.map((d) => [xScale(d.x), yScale(d.y)]);
+        return Delaunay.from(formattedData);
+    }, [props.data, xScale, yScale]);
 
+    const voronoi = useMemo(() => {
+        return delaunay.voronoi([0, 0, props.width, props.height]);
+      }, [props.width, props.height, delaunay]);
 
-    const toggleArmyViewer = (e, armyId) => {
-        const overlayRect = e.target.getBoundingClientRect();
-        const position = {
-            x: overlayRect.left + window.scrollX,
-            y: overlayRect.top + window.scrollY
-        };
-
-        setActiveArmyViewers(prev => {
-            const index = prev.findIndex(viewer => viewer.id === armyId);
-            if (index >= 0) {
-                // Remove viewer if already active
-                return prev.filter(viewer => viewer.id !== armyId);
-            } else {
-                return [...prev, {id: armyId, position}];
-            }
+    const renderClippedImages = () => {
+        return props.data.map((d, i) => {
+            const regionPath = voronoi.renderCell(i);
+            const imagePath = getImagePath(d.regionType);
+            return (
+                <g key={`group-${i}`}>
+                    <clipPath id={`clip-${i}`}>
+                        <path d={regionPath} />
+                    </clipPath>
+                    <image
+                        key={`image-${i}`}
+                        xlinkHref={imagePath}
+                        // href={imagePath}
+                        clipPath={`url(#clip-${i})`}
+                        width={props.width}
+                        height={props.height}
+                        onError={(e) => console.error(`Error loading image: ${imagePath}`, e)}
+                    />
+                </g>
+            );
         });
     };
 
-    const updateArmyPosition = (armyId, newX, newY) => {
-        setArmyImages(currentArmyImages => currentArmyImages.map(army => {
-            if (army.id === armyId) {
-                return {...army, x: newX, y: newY};
-            }
-            return army;
-        }));
-        setUpdateTrigger(prev => !prev)
-    };
-
-    useEffect(() => {
-        loadImage(props.mapImage, setImage);
-    }, [props.mapImage]);
-
-    useEffect(() => {
-        loadImage(props.mapImage, setImage)
-    }, [props.mapImage])
-
-    {/*Get images of cities on map cities on the map*/}
-    const [selectedCityId, setSelectedCityId] = useState(null);
-    const [showCities, setShowCities] = useState(true);
-    const [citiesLoaded, setCitiesLoaded] = useState(false)
-
-    const handleCityClick = (cityId) => {
-        setSelectedCityId(cityId);
-        setShowCityManager(true);
-        setShowCities(false);
-    };
-    const [cityImages,setCityImages] = useState([]);
-    {/*Load cities from databank, and get images*/}
-    useEffect(() => {
-        const fetchCities = async () => {
-            const cities = await getCities(1);
-
-            // replace with actual planetID
-            const cityElements = cities.map(city => ({
-                ...city,
-                onClick: () => handleCityClick(city.id),
-            }));
-            setCityImages(cityElements);
-            setCitiesLoaded(true);
-        };
-        if (!citiesLoaded) {
-            fetchCities();
-        }
-    }, [props.planetId, handleCityClick, citiesLoaded]);
-
-    {/*handle closing of cityManager window*/}
-    const [showCityManager, setShowCityManager] = useState(true);
-    const handleCloseCityManager = () => {
-        setShowCityManager(false);
-        setSelectedCityId(null);
-        setShowCities(true);
-    }
-
-    const [planetList, setPlanetList] = useContext(PlanetListContext)
-    const [planetListIndex, setPlanetListIndex] = props.planetListIndex;
-    useEffect(() => {
-        const fetchArmies = async () => {
-            const armies = await getArmies(1);
-            const armyElements = armies.map(army => ({
-                ...army,
-                onClick: (e) => toggleArmyViewer(e, army.id),
-            }));
-            setArmyImages(armyElements);
-        };
-        fetchArmies();
-
-    }, [updateTrigger]); // get the armies again when an army has been moved
-
-
     return (
-        <>
-        <div className="bg-gray-800 mx-auto w-2/12 py-3 fixed inset-x-0 top-5 z-10 border-2 border-white md:text-3xl justify-between items-center flex z-30">
-        <RiArrowLeftSLine className="transition ease-in-out hover:scale-150" onClick={() => {let new_id = planetListIndex-1; if (new_id < 0){new_id+= planetList.length;} setPlanetListIndex(new_id)}}/>
-         <h1>{props.planetName}</h1>
-         <RiArrowRightSLine className="transition ease-in-out hover:scale-150" onClick={() => {let new_id = planetListIndex+1; new_id = new_id % planetList.length; setPlanetListIndex(new_id)}}/>
-         </div>
-
-            {
-                activeArmyViewers.map(({id, position}) => (
-                    <div key={id} style={{
-                        position: 'absolute',
-                        left: `${position.x}px`,
-                        top: `${position.y}px`,
-                    }}>
-                        <ArmyViewer armyId={id} onUpdatePosition={updateArmyPosition}/>
-                    </div>
-                ))
-            }
-
-        {
-        image &&
-            <MapInteractionCSS
-                    value={mapState}
-                    onChange={(value) => setMapState(value)}
-                    minScale={1}
-                    maxScale={5}
-                    translationBounds={{
-                        xMin: image.width - mapState.scale * image.width,
-                        xMax: 0,
-                        yMin: image.height - mapState.scale * image.height,
-                        yMax: 0,
-                    }}
-                >
-                    <img src={image.src} alt="map"
-                         style={{imageRendering: "pixelated", width: "100vw", height: "auto"}}/>
-                    {armyImages.map((army, index) => (
-                        <img key={index} src={army.src} alt="army" style={army.style}
-                             onClick={(e) => toggleArmyViewer(e, army.id)}/>
-                    ))}
-
-                    {/*Display cities on the map*/}
-                        {showCities && cityImages.map((city, index) => (
-                          <img key={index} src={city.src} alt="city" style={city.style} onClick={city.onClick} />
-                        ))}
-
-                    {/*Display cityManager over the map*/}
-                    {selectedCityId && showCityManager && (
-                            <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 20 }}>
-                                <CityManager cityId={selectedCityId} primaryColor="black" secondaryColor="black" onClose={handleCloseCityManager} />
-                            </div>
-                    )}
-
-                </MapInteractionCSS>
-
-        }
-
-        </>
+        <svg width={props.width} height={props.height}>
+            {renderClippedImages()}
+            <path key={`voronoi-total`} d={voronoi.render()} stroke="orange" fill="none" />
+        </svg>
     );
 }
 

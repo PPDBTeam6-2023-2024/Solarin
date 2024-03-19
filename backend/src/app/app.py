41e2675af70e671873358a40b97ec03835d68a39
emiilll <emil.lambert@student.uantwerpen.be>
2024-03-19 20:36:51 +0100

    added army router to app.py

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .routers.authentication.router import router as auth_router
 from .routers.chat.router import router as chat_router
+from .routers.army.router import router as army_router
 from .database.database import sessionmanager
 from .config import APIConfig
 from .customize_logger import CustomizeLogger
@@ -24,6 +25,7 @@ def init_app(config: APIConfig) -> FastAPI:
 
     app.include_router(auth_router)
     app.include_router(chat_router)
+    app.include_router(army_router)
 
     if config.db:
         sessionmanager.init(config.db.get_connection_string().get_secret_value())

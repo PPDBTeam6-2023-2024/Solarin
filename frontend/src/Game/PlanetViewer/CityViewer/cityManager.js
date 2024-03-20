4bd62954fac7afddc5e5924bfeb0e0cb3fba6601
Dio358 <125569095+Dio358@users.noreply.github.com>
2024-03-20 09:51:19 +0100

    Optimisation basic cities

--- frontend/src/Game/PlanetViewer/CityViewer/cityManager.js
+++ frontend/src/Game/PlanetViewer/CityViewer/cityManager.js
@@ -1,20 +1,18 @@
-import React, {useState, useEffect, useMemo, createElement} from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import './cityManager.css'
+import './cityManager.css';
 import Draggable from "react-draggable";
-import barracks from "../../Images/building_images/Barracks.png"
-import mine from "../../Images/building_images/Mine.png"
-import factory from "../../Images/building_images/Factory.png"
-import shipyard from "../../Images/building_images/Shipyard.png"
-import axios, {request} from "axios";
-import app from "../../../App";
-
+import barracks from "../../Images/building_images/Barracks.png";
+import mine from "../../Images/building_images/Mine.png";
+import factory from "../../Images/building_images/Factory.png";
+import shipyard from "../../Images/building_images/Shipyard.png";
+import axios from "axios";
 
 const getBuildings = async (cityId) => {
     try {
-        axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.getItem('access-token')}`}
+        axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.getItem('access-token')}`};
         const response = await axios.get(`${process.env.REACT_APP_BACKEND_PATH}/cityManager/buildings?city_id=${cityId}`);
         console.log('Response data:', response.data);
         if (response.status === 200 && Array.isArray(response.data)) {
@@ -26,70 +24,69 @@ const getBuildings = async (cityId) => {
         return [];
     }
 };
+
 const CityManager = ({ cityId, primaryColor, secondaryColor, onClose }) => {
-    //get all buildings
     const [rowData, setRowData] = useState([]);
     const [selectedImage, setSelectedImage] = useState(null);
-
     const [initialClick, setInitialClick] = useState(true);
+    const [buildings, setBuildings] = useState([]);
 
-    const columns = useMemo(() =>[
-        { headerName: "Building Type", field: "buildingType"},
+    const columns = useMemo(() => [
+        { headerName: "Building Type", field: "buildingType" },
         { headerName: "Building Rank", field: "buildingRank" },
         { headerName: "Resource Timer", field: "resourceTimer" }
     ]);
 
     useEffect(() => {
-        getBuildings(cityId).then(buildings => {
-            setRowData(buildings.map(building => ({
-                buildingType: building.building_type,
-                buildingRank: building.rank,
-                resourceTimer: 'Unknown',
-                image: getImageForBuildingType(building.building_type)
-            })));
-        });
-    }, [cityId]);
+        if (cityId && buildings.length === 0) {
+            getBuildings(cityId).then(buildings => {
+                setBuildings(buildings);
+                setRowData(buildings.map(building => ({
+                    buildingType: building.building_type,
+                    buildingRank: building.rank,
+                    resourceTimer: 'Unknown',
+                    image: getImageForBuildingType(building.building_type)
+                })));
+            });
+        }
+    }, [cityId, buildings]);
 
     const getImageForBuildingType = (buildingType) => {
         switch (buildingType) {
             case 'barracks':
                 return barracks;
-            case 'The mines of moria':
+            case 'reactor':
                 return mine;
-            case 'factory':
+            case 'nexus':
                 return factory;
-            case 'shipyard':
+            case 'space-dock':
                 return shipyard;
             default:
-                return ''; // Default image or empty string if no match
+                return barracks;
         }
     };
-        {/*Event handler for row mouse over*/}
-      const onRowMouseOver = event => {
-        setSelectedImage(event.data.image);
-        console.log(selectedImage);
-        getBuildings().then();
-      };
 
-        useEffect(() => {
+    const onRowMouseOver = event => {
+        setSelectedImage(event.data.image);
+    };
+
+    useEffect(() => {
         const handleClickOutside = event => {
             const { target } = event;
             const agGridElement = document.querySelector('.ag-theme-alpine-dark.ag-grid-container');
             const selectedImageElement = document.querySelector('.selected-image');
 
             if (agGridElement.contains(target) || (selectedImageElement && selectedImageElement.contains(target))) {
-                // Click inside AG Grid or on the selected image, do nothing
                 return;
             }
 
-            if (!initialClick){
-                console.log("Clicked outside AG Grid and the selected image!");
+            if (!initialClick) {
+                {/*case: Clicked outside AG Grid and the selected image*/}
                 onClose();
             } else {
-                console.log("initial click logged");
+                {/*case: log initial click*/}
                 setInitialClick(false);
             }
-
         };
 
         document.addEventListener('click', handleClickOutside);
@@ -113,7 +110,7 @@ const CityManager = ({ cityId, primaryColor, secondaryColor, onClose }) => {
                 </Draggable>
             </div>
             <div className="right-container">
-                {selectedImage && <img src={selectedImage} alt="Building" className="selected-image"  />}
+                {selectedImage && <img src={selectedImage} alt="Building" className="selected-image" />}
             </div>
         </div>
     );

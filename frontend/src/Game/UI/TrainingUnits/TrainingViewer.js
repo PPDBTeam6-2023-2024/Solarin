ecce407ffae2d0b51889a10ff82817c124a928d3
Dio358 <125569095+Dio358@users.noreply.github.com>
2024-04-09 23:28:19 +0200

    TrainingViewer Fix

--- frontend/src/Game/UI/TrainingUnits/TrainingViewer.js
+++ frontend/src/Game/UI/TrainingUnits/TrainingViewer.js
@@ -67,8 +67,9 @@ function TrainingViewer({building_id, onClose}) {
             scroll_bar.current.scrollLeft += evt.deltaY;
         });
 
+
         const handleClickOutside = (event) => {
-            if (scroll_bar.current && !scroll_bar.current.contains(event.target)) {
+            if ((scroll_bar.current && !scroll_bar.current.contains(event.target)) && !event.target.closest('.TrainingOptionList')) {
                 onClose();
             }
         };
@@ -80,7 +81,7 @@ function TrainingViewer({building_id, onClose}) {
         };
 
 
-    }, [scroll_bar, onClose]);
+    }, [scroll_bar]);
 
 
     return (

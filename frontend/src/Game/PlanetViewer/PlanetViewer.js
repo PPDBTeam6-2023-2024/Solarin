23273312cde345d3b9c6a61969e164b4b7dead28
tibov <tibo.verreycken@telenet.be>
2024-04-15 17:27:00 +0200

    fixed city disappear bug

--- frontend/src/Game/PlanetViewer/PlanetViewer.js
+++ frontend/src/Game/PlanetViewer/PlanetViewer.js
@@ -212,7 +212,7 @@ function PlanetViewer(props) {
                         handleCityClick: handleCityClick,
                         setCityImages: setCityImages,
                         setCitiesLoaded: setCitiesLoaded
-                    });
+                    }, props.planetId);
                     socket.send(
                         JSON.stringify(
                             {

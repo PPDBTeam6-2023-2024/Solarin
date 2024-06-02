7a760da064d92f1923dbcb26c46cef7e0ab03cce
tibov <tibo.verreycken@telenet.be>
2024-06-02 09:30:06 +0200

    fix production (hopefully)

--- backend/src/app/__main__.py
+++ backend/src/app/__main__.py
@@ -3,5 +3,7 @@ import uvicorn
 from . import config
 from .app import init_app
 
+app = init_app(config)
+
 if __name__ == "__main__":
-    uvicorn.run(app=init_app(config), host="0.0.0.0", port=8000)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)

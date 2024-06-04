5909efe871de1829869287a47e25bacae4d535c7
tibov <tibo.verreycken@telenet.be>
2024-06-04 17:42:58 +0200

    will this cahnge anything about CORS

--- frontend/src/Game/Game.js
+++ frontend/src/Game/Game.js
@@ -88,7 +88,7 @@ const Game = () => {
             axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.getItem('access-token')}`,
             'content-type': 'application/x-www-form-urlencoded',
             'accept': 'application/json'}
-            const response2 = await axios.get(`${process.env.REACT_APP_BACKEND_PATH}/spawn/`)
+            const response2 = await axios.get(`${process.env.REACT_APP_BACKEND_PATH}/spawn`)
 
             const response = await axios.get(`${process.env.REACT_APP_BACKEND_PATH}/planet/planets/private`)
 
